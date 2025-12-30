d1aa61d4e7938513e9b9e2b328dcc07c2314e97f
jeong-minG <ika818488@gmail.com>
2025-12-30 23:55:51 +0900

    feat:post-modal

--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -1,18 +1,46 @@
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { ImageIcon } from "lucide-react";
+import { usePostEditorModal } from "@/store/post-editor-modal";
+import { useEffect, useRef, useState } from "react";
 
 export default function PostEditorModal() {
+  const { isOpen, close } = usePostEditorModal();
+  const [content, setContent] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + "px";
+    }
+  }, [content]);
+  useEffect(() => {
+    if (!isOpen) return;
+    textareaRef.current?.focus();
+    setContent("");
+  }, [isOpen]);
+  const handleCloseModal = () => {
+    close();
+  };
+
   return (
-    <Dialog>
-      <DialogContent>
+    <Dialog open={isOpen} onOpenChange={handleCloseModal}>
+      <DialogContent className="max-h-[90vh]">
         <DialogTitle>포스트 작성</DialogTitle>
-        <textarea />
-        <Button>
+        <textarea
+          ref={textareaRef}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="max-h-125 min-h-25 focus:outline-none"
+          placeholder="무슨 일이 있었나요?"
+        />
+        <Button variant={`outline`} className="cursor-pointer">
           <ImageIcon />
           이미지 추가
         </Button>
-        <Button>저장</Button>
+        <Button className="cursor-pointer">저장</Button>
       </DialogContent>
     </Dialog>
   );
