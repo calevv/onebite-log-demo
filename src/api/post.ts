99c98a167af700aa50a9e25f1b4427a7a4c543db
calevv <ika818488@gmail.com>
2025-12-31 17:27:12 +0900

    feat:포스트 테이블 업데이트

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,7 +1,64 @@
 import supabase from "@/lib/superbase";
+import { uploadImage } from "./image";
+import type { PostEntity } from "@/types";
 
 export async function createPost(content: string) {
-  const { data, error } = await supabase.from("post").insert({ content });
+  const { data, error } = await supabase
+    .from("post")
+    .insert({ content })
+    .select()
+    .single();
+
+  if (error) throw error;
+
+  return data;
+}
+
+export async function createPostWithImages({
+  content,
+  images,
+  userId,
+}: {
+  content: string;
+  images: File[];
+  userId: string;
+}) {
+  // 1. 새로운 포스트 생성
+  const post = await createPost(content);
+
+  if (images.length === 0) return post;
+
+  // 2. 이미지 업로드
+  // 병렬처리 만들기
+
+  const imageUrls = await Promise.all(
+    images.map((image) => {
+      const fileExtension = image.name.split(".").pop() || "webp";
+      const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExtension}`;
+      const filePath = `${userId}/${post.id}/${fileName}`;
+      return uploadImage({
+        file: image,
+        filePath,
+      });
+    }),
+  );
+
+  // 3. 포스트 테이블 업데이트
+  const updatedPost = await updatePost({
+    id: post.id,
+    image_urls: imageUrls,
+  });
+
+  return updatedPost;
+}
+
+export async function updatePost(post: Partial<PostEntity> & { id: number }) {
+  const { data, error } = await supabase
+    .from("post")
+    .update(post)
+    .eq("id", post.id)
+    .select()
+    .single();
 
   if (error) throw error;
 
